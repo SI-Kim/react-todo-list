8df7d2193a9bef4566644b0955df5a0e3a6dece8
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2022-04-15 17:50:23 +0900

    header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,3 +1,18 @@
 export default function Header() {
-  return <div className="header">this is header area.</div>;
+  return (
+    <div
+      className="header"
+      style={{ border: "solid 1px", backgroundColor: "lightgray" }}
+    >
+      <span className="logo">logo</span>
+      &nbsp;&nbsp;&nbsp;this is header area.
+      <nav className="nav">
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
